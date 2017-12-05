4ee6fe49066d24cf89549f64b8bcb96b74ce3f73
finbarrobrien <finbarr.obrien@gmail.com>
2017-12-05 21:30:11 +0000

    Animate floating action button

--- FloatingActionButton.js
+++ FloatingActionButton.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { StyleSheet, Image, View, TouchableNativeFeedback } from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  View,
+  TouchableNativeFeedback,
+  Animated,
+  Easing,
+} from 'react-native';
 
 const Button = StyleSheet.create({
   roundButton: {
@@ -14,14 +21,35 @@ const Button = StyleSheet.create({
 });
 
 export default class FloatingActionButton extends React.Component {
+  state = {
+    scale: new Animated.Value(0), // Initial value for opacity: 0
+  };
+
   static defaultProps = {
     src: '',
     elevation: 2,
-    radius: 56,
+    radius: 28,
     color: '',
     onPress: () => {},
-    size: 'normal' // 'normal' or 'small'
+    small: false,
+    size: 'normal', // 'normal' or 'small'
   };
+
+  componentDidMount() {
+    Animated.timing(this.state.scale, {
+      toValue: 1,
+      easing: Easing.in(),
+      duration: 250,
+    }).start();
+  }
+
+  componentWillUnmount() {
+    Animated.timing(this.state.scale, {
+      toValue: 0,
+      easing: Easing.in(),
+      duration: 250,
+    }).start();
+  }
 
   render() {
     const {
@@ -41,38 +69,49 @@ export default class FloatingActionButton extends React.Component {
     if (color) {
       buttonStyle.push({ backgroundColor: this.props.color });
     }
-    if(elevation) {
+    if (elevation) {
       buttonStyle.push({ elevation: this.props.elevation });
     }
     if (radius) {
       buttonStyle.push({
-        width: this.props.radius,
-        height: this.props.radius,
-        borderRadius: this.props.radius,
+        width: 2 * this.props.radius,
+        height: 2 * this.props.radius,
+        borderRadius: 2 * this.props.radius,
       });
     } else {
-      if (this.props.small) {
-        buttonStyle.push({
-          width: this.props.small ? 40 : 56,
-          height: this.props.small ? 40 : 56,
-          borderRadius: this.props.small ? 40 : 56,
-        });
-      }
+      buttonStyle.push({
+        width: this.props.small ? 20 : 28,
+        height: this.props.small ? 20 : 28,
+        borderRadius: this.props.small ? 20 : 28,
+      });
     }
+    buttonStyle.push({ transform: [{ scale: this.state.scale }]});
 
     return (
-      <TouchableNativeFeedback
-        accessibilityComponentType="button"
-        accessibilityLabel={accessibilityLabel}
-        accessibilityTraits={accessibilityTraits}
-        testID={testID}
-        disabled={disabled}
-        onPress={onPress}
-      >
-        <View style={buttonStyle}>
-          <Image style={Button.icon} source={{ uri: src }} />
-        </View>
-      </TouchableNativeFeedback>
+      <Animated.View style={buttonStyle}>
+        <TouchableNativeFeedback
+          accessibilityComponentType="button"
+          accessibilityLabel={accessibilityLabel}
+          accessibilityTraits={accessibilityTraits}
+          testID={testID}
+          disabled={disabled}
+          onPress={onPress}
+          background={TouchableNativeFeedback.Ripple('#ffffff', true)}
+        >
+          <View
+            style={{
+              width: 56,
+              height: 56,
+              borderRadius: 28,
+              flex: 0,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Image style={Button.icon} source={{ uri: src }} />
+          </View>
+        </TouchableNativeFeedback>
+      </Animated.View>
     );
   }
 }
